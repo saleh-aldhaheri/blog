e791299eca1d189983b316156704d7bb53c75c42
Triple Y IT Engineer 04 <tripleyit04@tripleystudio.com>
2026-04-16 22:46:31 +0800

    configurate the routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,25 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::prefix('api/v1')
+                ->as('api.')
+                ->group(function () {
+                    Route::prefix('admin')
+                        ->as('admin.')
+                        ->group(base_path('routes/api/v1/admin.php'));
+
+                    Route::as('user.')
+                        ->group(base_path('routes/api/v1/user.php'));
+                });
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
