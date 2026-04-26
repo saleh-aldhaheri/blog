7bd88df1ba28d003d8a1ad0d80619f8b7da5ed8d
Saleh Ahmed <salehaldhaheri@09gmail.com>
2026-04-26 17:12:39 +0800

    fix the interactions

--- app/Http/V1/Resources/InteractionResource.php
+++ app/Http/V1/Resources/InteractionResource.php
@@ -15,6 +15,7 @@ class InteractionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'user_id' => $this->user_id,
             'action' => $this->action,
             'interactable_type' => $this->interactable_type,
